2e1360dea7bda7250cef030c685e4e6ae08e99db
Tobias Lafer <tobiaslafer@localhost.localdomain>
2021-04-01 14:05:01 +0200

    Add dumping sensor-data do csv-files

--- app/src/main/java/com/example/mobilecomputingassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputingassignment/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.mobilecomputingassignment
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
-import android.bluetooth.*
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import java.io.*
 import java.util.*
+
+
+// Libraries for File IO
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+
+    // ========== Sensor-data stuff
     private lateinit var mSensorManager: SensorManager;
 
     private lateinit var mGyroscope: Sensor;
@@ -24,14 +34,30 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var twGyroZ: TextView;
     private lateinit var twProximity: TextView;
 
+    private var lApplicationStartTime: Long = Calendar.getInstance().timeInMillis;
+
+    // Sensor data. First column: timestamps, second column: sensor-values
+    private val arrSensorData = arrayOf(
+            floatArrayOf(0.0f, 0.0f, 0.0f, 0.0f, 0.0f),
+            floatArrayOf(0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+    );
+
+
+    // ========== Bluetooth stuff
     private lateinit var bluetoothManager: BluetoothManager
     private lateinit var bluetoothAdapter: BluetoothAdapter;
     private lateinit var connectedBluetoothDevice: BluetoothDevice;
     private lateinit var bluetoothSocket: BluetoothSocket;
 
     private val REQUEST_ENABLE_BT: Int = 1;
-
     private val bluetoothUUID = UUID.fromString("53eef5bf-6702-4ab6-bcf3-ae02a401d70e");
+
+    private lateinit var inputStream: InputStream;
+    private lateinit var outputStream: OutputStream;
+
+    // ========== FileIO Stuff
+    private lateinit var fGyroSensorDataFile: FileWriter;
+    private lateinit var fProximitySensorDataFile: FileWriter;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,22 +95,56 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
 
 
+        /* ========== Establish bluetooth connection ========== */
+        /*
         bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager;
         bluetoothAdapter = bluetoothManager.adapter
 
-
-        if (bluetoothAdapter.isEnabled == false) {
+        // Enable bluetooth adapter if necessary
+        if (bluetoothAdapter.isEnabled) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
 
+        // This is ugly. Improve it...
         val connectedBluetoothDevices: MutableSet<BluetoothDevice> =
                 bluetoothAdapter.bondedDevices;
-
         connectedBluetoothDevice = connectedBluetoothDevices.elementAt(0);
 
         bluetoothSocket = connectedBluetoothDevice.createRfcommSocketToServiceRecord(bluetoothUUID);
         bluetoothSocket.connect();
+
+        // Finally, get the streams for sending and receiving data
+        inputStream = bluetoothSocket.inputStream;
+        outputStream = bluetoothSocket.outputStream;
+         */
+
+        // ========== Create Sensor-Data File
+        createSensorDataFiles()
+        
+    }
+
+    private fun createSensorDataFiles() {
+        val gyroSensorDataFilePath = File(filesDir, "gyro_sensor_data.txt");
+        try {
+            fGyroSensorDataFile = FileWriter(gyroSensorDataFilePath);
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace();
+        } catch (e: IOException) {
+            e.printStackTrace();
+        }
+
+        val proximitySensorDataFilePath = File(filesDir, "proxy_sensor_data.txt");
+        try {
+            fProximitySensorDataFile = FileWriter(proximitySensorDataFilePath);
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace();
+        } catch (e: IOException) {
+            e.printStackTrace();
+        }
+
+        fGyroSensorDataFile.write("time;x;y;z\n");
+        fProximitySensorDataFile.write("time;value\n");
     }
 
     override fun onStart() {
@@ -97,6 +157,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onStop()
         mSensorManager.unregisterListener(this, mGyroscope);
         mSensorManager.unregisterListener(this, mProximity);
+
+        fGyroSensorDataFile.flush()
+        fProximitySensorDataFile.flush()
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -115,10 +178,25 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             twGyroX.text = resources.getString(R.string.gyroscope_label_x, fGyroX);
             twGyroY.text = resources.getString(R.string.gyroscope_label_y, fGyroY);
             twGyroZ.text = resources.getString(R.string.gyroscope_label_z, fGyroZ);
+
+            val fTimestamp: Float = (Calendar.getInstance().timeInMillis - lApplicationStartTime).toFloat();
+            arrSensorData[0][0] = fTimestamp;
+            arrSensorData[0][1] = fTimestamp;
+            arrSensorData[0][2] = fTimestamp;
+
+            arrSensorData[1][0] = fGyroX;
+            arrSensorData[1][1] = fGyroY;
+            arrSensorData[1][2] = fGyroZ;
+
+            fGyroSensorDataFile.write("%f;%f;%f;%f\n".format(fTimestamp, fGyroX, fGyroY, fGyroZ));
         }
 
         if (iSensorType == Sensor.TYPE_PROXIMITY) {
             twProximity.text = resources.getString(R.string.proximity_label, event.values[0]);
+
+            val fTimestamp: Float = (Calendar.getInstance().timeInMillis - lApplicationStartTime).toFloat();
+            val fSensorValue: Float = event.values[0];
+            fProximitySensorDataFile.write("%f;%f\n".format(fTimestamp, fSensorValue));
         }
     }
 
