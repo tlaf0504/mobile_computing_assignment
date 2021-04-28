644d8680a8377046bb04c61544ce39e0b04a2eab
tlaf0504 <tobias.lafer@eclipso.at>
2021-04-28 09:37:13 +0200

    Enhance data preprocessing

--- Notebooks_Scripts_etc/ActivityRecognition/notebooks/utils.py
+++ Notebooks_Scripts_etc/ActivityRecognition/notebooks/utils.py
@@ -54,13 +54,47 @@ def plot_sensor_data(data_array:np.ndarray, title:str, ylabels:str, fig=None, cu
     plt.show()
     return fig_
 
-def load_and_resample_data(path:str, src_type="txt"):
+def load_data(path:str, src_type="txt"):
     if src_type == "txt":
         data = np.loadtxt(path)
     elif src_type == "npy":
         data = np.load(path, allow_pickle=True)
     else:
         raise Exception("File-type {:s} not implemented.".format(src_type))
-
-    data[:,0] = data[:,0] * 1e-3 # Convert milli-seconds to seconds
+    
     return data
+
+def clip_data(t, signals, T_start, T_end):
+    idx_clip_start = np.where(t >= T_start)[0][0]
+    idx_clip_end = np.where(t <= T_end)[0][-1]
+
+    return idx_clip_start, idx_clip_end, t[idx_clip_start:idx_clip_end + 1], signals[idx_clip_start:idx_clip_end + 1, :]
+
+
+def separate_and_save_testset_timeframes(t:np.ndarray, signals:np.ndarray, frame_width:float, filename_template:str):
+    t_start = t[0]
+    t_end = t[-1]
+
+    t_frame_start = t_start
+    t_frame_end = t_frame_start + frame_width
+    frame_idx = 0
+    
+    while t_frame_end <= t_end:
+        idx_start = np.where(t >= t_frame_start)[0][0]
+        idx_end = np.where(t <= t_frame_end)[0][-1]
+
+        frame = signals[idx_start:idx_end+1, :]
+        t_frame = t[idx_start:idx_end+1]
+
+        filename = filename_template + f"{frame_idx}.csv"
+        print(f"Writing file to {filename}...", end="")
+        np.savetxt(filename, np.column_stack((t_frame, frame)), fmt="%e", delimiter=";")
+        print("Done")
+
+        t_frame_start = t_frame_end
+        t_frame_end = t_frame_start + frame_width
+        frame_idx = frame_idx + 1
+
+        
+
+
